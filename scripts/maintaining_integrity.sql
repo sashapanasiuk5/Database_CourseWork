3ec2a264ab34994701081144eb1df834c5b8a61a
sashapanasiuk5 <51517423+sashapanasiuk5@users.noreply.github.com>
2023-12-28 03:13:18 +0200

    add description of the ER-diagram

--- scripts/maintaining_integrity.sql
+++ scripts/maintaining_integrity.sql
@@ -4,6 +4,9 @@ ADD CONSTRAINT date_check CHECK(inspection_date <= CURRENT_DATE);
 ALTER TABLE cars
 ADD CONSTRAINT year_check CHECK(year_of_manufacture <= date_part('year', CURRENT_DATE));
 
+ALTER TABLE cars
+ADD CONSTRAINT unique_license_plate UNIQUE(license_plate)
+
 ALTER TABLE details
 ADD CONSTRAINT cost_check CHECK(cost > 0);
 
